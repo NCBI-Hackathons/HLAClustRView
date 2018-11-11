760eec17824f750bda4ca10b6b774eaada331ad5
Adewunmi91 <aadelaja@ucla.edu>
2018-11-11 15:13:52 -0500

    Update splitTyping

--- R/splitTyping.R
+++ R/splitTyping.R
@@ -2,11 +2,13 @@ splitTyping <- function (aChar)
 {
     library (stringr)
     options('stringAsFactor'=FALSE)
-    aChar<-"A*31:01:02:01"
-    prefix <-str_extract(aChar, '(^.*[:digit:])|(^[:alpha:]+$)')
-    splitted <- strsplit(prefix,'[.*:_]')
-    suffix <-str_extract(aChar, '(?<=([:digit:]|[.*:_]))[:alpha:]+$')
-    output <- append(unlist(splitted), suffix)
+    prefix <-str_extract(aChar, "(^.*[:digit:])|(^[:alpha:]+$)")
+    splitted <- strsplit(prefix,"[.*:_]")
+    pad <-5-lengths(splitted)
+    splitted <- append(splitted,rep(NA, pad) )
+    suffix <-str_extract(aChar, "(?<=([:digit:]|[.*:_]))[:alpha:]+$")
+    output <- c(unlist(splitted), suffix)
+
     return(output)
 
 }
