894d3f28da22e1f9cf23fcdf7d14ffdee58a4a25
Astrid Deschenes <adeschen@hotmail.com>
2018-11-12 15:57:10 -0500

    Update doc and code to remove all errors and warnings; Update author list

--- R/draw_cluster.R
+++ R/draw_cluster.R
@@ -1,48 +1,63 @@
-#' Convert distance table to distance matrix
+#' @title Convert distance table to distance matrix
 #'
-#' This function takes the output of \code{calculateSimilarity} function
-#' and returns a distance matrix to use for clustering
-#' @param outMet tibble with hamming distance for sample pairs \code{calculateSimilarity}
+#' @description This function takes the output of \code{calculateSimilarity}
+#' function and returns a distance matrix to use for clustering
 #'
-#' @return
-#' @export
+#' @param outMet tibble with hamming distance for sample
+#'     pairs \code{calculateSimilarity}
+#'
+#' @return TODO
 #'
 #' @examples
+#'
+#' ##TODO
+#'
+#' @author Santiago Medina, Pascal Belleau
+#' @export
 make_distance_matrix <- function(outMet) {
     nbCase <- length(unique(outMet$SampleName1)) + 1
     matDia <- matrix(unlist(sapply(seq_len(nbCase),
                                    FUN=function(x,outMetric, nbCase){
                                        l<-NULL
                                        if(x < nbCase){
-                                           l <- c(rep(0,x),
+                                           l <- c(rep(0, x),
                                                   outMetric$HammingDistance[
                                                       ((x-1) * (nbCase-1) - ((x-1)*x)/2 + 1):
                                                           (x * (nbCase-1) - (x*(x+1))/2 +1)])
                                        } else{
-                                           l <- rep(0,x)
+                                           l <- rep(0, x)
                                        }
 
                                        return(l)
-                                   }, outMetric=outMet,nbCase=nbCase)), nc=nbCase )
+                                   }, outMetric=outMet, nbCase=nbCase)), ncol=nbCase )
 
-    # add row names and column names to the distance matrix
-    nameMat <- c(unique(unlist(outMet[,1])), unique(unlist(outMet[,2]))[length(unique(unlist(outMet[,2])))])
+    # Add row names and column names to the distance matrix
+    nameMat <- c(unique(unlist(outMet[,1])),
+                 unique(unlist(outMet[,2]))[length(unique(unlist(outMet[,2])))])
     rownames(matDia) <- nameMat
     colnames(matDia) <- nameMat
-    matDia
 
+    return(matDia)
 }
 
-#' Draw cluster
+#' @title Draw cluster
+#'
+#' @description TODO
 #'
 #' @param matrixDistance distance matrix
+#'
+#' @return TODO
+#'
+#' @examples
+#'
+#' ##TODO
+#'
+#' @author Santiago Medina, Pascal Belleau
+#'
 #' @importFrom dplyr %>%
 #' @importFrom graphics plot
 #' @importFrom stats as.dist
-#' @return
 #' @export
-#'
-#' @examples
 draw_cluster <- function(matrixDistance) {
     hc <- as.dist(matrixDistance) %>%
         hclust()
