3536452b14b93a40a063503fa8d502fe83e6f84e
Adewunmi91 <aadelaja@ucla.edu>
2018-11-11 19:52:23 -0500

    parser saves to RDS instead of RData

--- R/parseHLADb.R
+++ R/parseHLADb.R
@@ -12,8 +12,8 @@ parseHLADb <-function (hlaDbPath)
 
      #check if RData file is present
     folderName <- dirname(hlaDbPath);
-    rdaFileName <-paste(folderName,'hladb.Rdata', sep = '/')
-    if (!file.exists(rdaFileName))
+    rdsFileName <-paste(folderName,'hladb.rds', sep = '/')
+    if (!file.exists(rdsFileName))
     {
     fileName <- paste(folderName, 'E_GEUVexp.txt',sep = '/')
     file.exists(fileName)
@@ -38,11 +38,11 @@ parseHLADb <-function (hlaDbPath)
     hla <-setClass('HLADb',
                   contains = c("data.frame","list")                  )
     db<-hla(db)
-    save(db, file= rdaFileName)
+    saveRDS(db, file= rdsFileName)
 
     }else
     {
-        db <-load(rdaFileName)
+        db <-readRDS(rdsFileName)
     }
     return (db)
 #
