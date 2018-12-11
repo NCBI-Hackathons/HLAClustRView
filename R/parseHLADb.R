a98abab0b4cf1d21d7fe8feef6a34313348e605f
Astrid Deschenes <adeschen@hotmail.com>
2018-12-10 20:32:24 -0500

    Change parameter name for readHLADataset() function.

--- R/parseHLADb.R
+++ R/parseHLADb.R
@@ -4,9 +4,9 @@
 #' multiple samples and transforms it into an
 #' HLADb object.
 #'
-#' @param hlaDbPath a \code{character} string, the filepath to a text file
+#' @param hlaFilePath a \code{character} string, the filepath to a text file
 #' containing the information about the samples typing. The name must
-#' correspond to an existing text file.
+#' correspond to an existing text file. The text file must have a header.
 #'
 #' @return a \code{data.frame} containing the following column names:
 #' \itemize{
@@ -30,7 +30,7 @@
 #' fileName <- paste0(directory, "/Samples_HLA_typing.txt")
 #'
 #' ## Parse file to extract HLA typing for all samples
-#' ex <- readHLADataset(hlaDbPath=fileName)
+#' ex <- readHLADataset(hlaFilePath=fileName)
 #'
 #' ## Show the first lines of the output dataset
 #' head(ex)
@@ -41,76 +41,48 @@
 #' @importFrom stringr str_split
 #' @importFrom utils read.table
 #' @export
-readHLADataset <-function (hlaDbPath)
+readHLADataset <-function (hlaFilePath)
 {
-    # GOAL: load information from HLA database and create a usable R
-    #  object that contains it
-    # Input validation:
-    #     must be valid database path
-    # minimum expected files must be present
-    db <- data.frame()
-
-     #check if RDS file is present
-    #folderName <- dirname(hlaDbPath)
-    #rdsFileName <-paste(folderName,'hladb.rds', sep = '/')
-    #if (!file.exists(rdsFileName))
-    #{
-
     ## Validate that the parameter is a character string
-    if (!is.character(hlaDbPath)) {
-        stop("The hlaDbPath parameter must by a character string")
+    if (!is.character(hlaFilePath)) {
+        stop("The hlaFilePath parameter must by a character string")
     }
 
     ## Validate that the parameter is a valid file
-    if (!file.exists(hlaDbPath)) {
-        stop(paste0("The file '", hlaDbPath, "' must be a valid text file"))
+    if (!file.exists(hlaFilePath)) {
+        stop(paste0("The file '", hlaFilePath, "' must be a valid text file"))
     }
 
-    #if (file.exists(hlaDbPath))
-    #{
-    data <- read.table(hlaDbPath, header = TRUE, stringsAsFactors = FALSE)
+    data <- read.table(hlaFilePath, header = TRUE, stringsAsFactors = FALSE)
     rownames(data) <- data[,1]
-    data <- data[,2:ncol(data)]
+    data <- data[, 2:ncol(data)]
 
     tData <- t(data)
     rNames <- rownames(tData)
     #split alleles numbers from gene name
     nameList <- rep(str_split(rNames, '_'), times = ncol(tData))
     sampleNames <- rep(colnames(tData), each = nrow(tData))
-    geneNames <-matrix(unlist(nameList),  ncol = 2, byrow=TRUE)
-    colName <-c('SampleName', 'AlleleName', 'GeneName', 'AlleleGroup',
+    geneNames <- matrix(unlist(nameList),  ncol = 2, byrow=TRUE)
+    colName <- c('SampleName', 'AlleleName', 'GeneName', 'AlleleGroup',
                     'Protein', 'SynSubst', 'NonCoding', 'Suffix')
 
-     #split the hla digits
+    ## Split the hla digits
     typing <- matrix(unlist(lapply(tData, splitTyping)), ncol = 6, byrow = TRUE)
-    db <- data.frame(cbind(matrix(sampleNames), geneNames[,2], typing),
+    db <- data.frame(cbind(matrix(sampleNames), geneNames[, 2], typing),
                         stringsAsFactors = FALSE)
     colnames(db) <- colName
 
-    #create HlADb class
-    #hla <-setClass('HLADb',
-    #              contains = c("data.frame"))
-    #db<-hla(db)
-
-    res<-list()
-    res[[1]]<- db
-    class(res) <- "HLADb"
-
-    #saveRDS(db, file= rdsFileName)
-    #}
-    #    else{
-    #        print("Can't find hlaDbPath.")
-    #    }
-    #
-    #}else
-    #{
-    #    db <-readRDS(rdsFileName)
-    #}
-
-    #
-    #remove samples with NA allele groups
+    ## Remove samples with NA allele groups
     drop <- db[is.na(db$AlleleGroup), 'SampleName']
-    db <- db[!db$SampleName %in% drop,]
+    db <- db[!db$SampleName %in% drop, ]
+
+    ## Format data.frame into tibble
+    db <- parse_hla_data(db)
+
+    ## Prepare HLADataset object
+    ##res <- list()
+    ##res[["data"]] <- db
+    ##class(res) <- "HLADataset"
 
     return(db)
 }
