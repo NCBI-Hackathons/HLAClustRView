abb275dbd00bff62e3ff49143f04c071fd642fad
Astrid Deschenes <adeschen@hotmail.com>
2019-01-22 16:05:50 -0500

    Format code to respect 80 characters line

--- R/splitTyping.R
+++ R/splitTyping.R
@@ -1,31 +1,36 @@
-#' @title
-#'splitTyping
-#' @description
-#'Converts a well-formed HLA allele name to a matrix of fields
-#' @param aChar
-#'Character array containing well-formed HLA allele
-#'see http://hla.alleles.org/nomenclature/naming.html
-#' @return
-#'n x 6 matrix of fields
+#' @title splitTyping
+#'
+#' @description Converts a well-formed HLA allele name to a matrix of fields
+#'
+#' @param aChar a \code{character} array containing one or more well-formed
+#' HLA alleles,
+#' see http://hla.alleles.org/nomenclature/naming.html
+#'
+#' @return a n x 6 matrix of fields
+#'
 #' @examples
-#'output <- splitTyping("HLA-DRB1*13:01:01:02")
-#'print(output)
-#'#[,1] [,2] [,3] [,4] [,5] [,6]
-#'#"HLA-DRB1" "13" "01" "01" "02" NA
+#'
+#' ## Split one HLA typing into an array of entries
+#' output <- splitTyping("HLA-DRB1*13:01:01:02")
+#'
+#' print(output)
+#'
+#' ## Split one HLA typing into an array of entries
+#' output <- splitTyping(c("G*01:04:01:02", "DQA1*01:01:01:03"))
+#'
+#' print(output)
 #'
 #' @author Adewunmi Adelaja
 #'
 #' @importFrom stringr str_extract
 #' @export
-splitTyping <- function (aChar)
-{
-    options('stringAsFactor'=FALSE)
-    prefix <-str_extract(aChar, "(^.*[:digit:])|(^[:alpha:]+$)")
+splitTyping <- function (aChar) {
+
+    prefix <- str_extract(aChar, "(^.*[:digit:])|(^[:alpha:]+$)")
     splitted <- strsplit(prefix,"[.*:_]")
-    splitted<-t(sapply(splitted, function(x) c(x,rep(NA, 5-length(x)))))
-    suffix <-str_extract(aChar, "(?<=([:digit:]|[.*:_]))[:alpha:]+$")
-    output <-cbind(splitted, as.array(suffix));
+    splitted <- t(sapply(splitted, function(x) c(x, rep(NA, 5 - length(x)))))
+    suffix <- str_extract(aChar, "(?<=([:digit:]|[.*:_]))[:alpha:]+$")
+    output <- cbind(splitted, as.array(suffix))
 
     return(output)
-
 }
