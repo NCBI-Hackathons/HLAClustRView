1b7b095dc848eef9e97c58b0b667484f31e553cb
Astrid Deschenes <adeschen@hotmail.com>
2019-02-07 19:03:09 -0500

    Update man page and add example to getSeqCMP() function

--- R/HLAdbTools.R
+++ R/HLAdbTools.R
@@ -1,26 +1,63 @@
-#' @title Get the sequence from the type
+#' @title Get the reference sequence and the sequences of two allele typing
+#' for a specific region.
 #'
-#' @description Get from the object HLAdb two sequences and the reference
-#' for a region
+#' @description From an \code{HLAdb} object that contains HLA aligment
+#' information for all typing, extract the reference sequence of a
+#' specific region of a HLA
+#' gene and the sequences associated to two allele typing for the same region.
+#' The two typing must be for the same HLA gene. This enable an aligment
+#' comparaison of the two allele typing.
 #'
-#' @param HLAInfo TODO
+#' @param HLAInfo an object of class \code{HLAdb} containing the global HLA
+#' aligment for all typing.
 #'
-#' @param regionExt TODO
+#' @param regionExt a \code{data.frame} containing a \code{start} and an
+#' \code{end} column. The \code{start} and \code{end} positions are both
+#' included in the extracted region.
 #'
-#' @param typeS1 Sample 1 TODO
+#' @param typeS1 a \code{character} string that represent the typing of the
+#' first sample. The \code{typeS1} and \code{typeS2} typing must be for the
+#' same HLA gene.
 #'
-#' @param typeS2 Sample 2 TODO
+#' @param typeS2 a \code{character} string that represent the typing of the
+#' second sample. The \code{typeS1} and \code{typeS2} typing must be for the
+#' same HLA gene.
 #'
-#' @return An object of class TODO
-#'
-#' @details TODO
+#' @return A \code{list} containing the following elements:
+#' \itemize{
+#' \item \code{data} a \code{tibble} object containing the HLA typing
+#' information for all samples. The columns are:
+#' \itemize{
+#' \item \code{refSeq} a \code{character} string that represent the
+#' reference sequence for the specified region of the HLA gene.
+#' \item \code{seqS1} a \code{character} string that represent the
+#' sequence of the first sample for the specified region of the allele typing.
+#' \item \code{seqS2} a \code{character} string that represent the
+#' sequence of the second sample for the specified region of the allele typing.
+#' }
+#' }
 #'
 #' @examples
 #'
-#' ## TODO
+#' ## Get path where some HLA database files are stored
+#' directory <- system.file("extdata", package = "HLAClustRView")
+#'
+#' ## Parse HLA database files of protein type
+#' HLAInfo <- parseHLADbAlignment(hlaDbPath=directory, seqType="prot")
+#'
+#' ## Select two allele typing for the same HLA gene
+#' sample1 <- "DRA*01:01:01:03"
+#' sample2 <- "DRA*01:02:03"
+#'
+#' ## Select a region to extract
+#' regions <- data.frame(start=c(160, 200), end=c(180, 220))
+#'
+#' ## Extract the aligment information for the specified region included the
+#' ## reference sequence and the sequences for both allele typing
+#' getSeqCMP(HLAInfo = HLAInfo, regionExt = regions,
+#'     typeS1 = sample1, typeS2 = sample2)
 #'
 #' @author Pascal Belleau, Astrid Deschenes
-#' @importFrom data.table data.table rbindlist
 #' @export
 getSeqCMP <- function(HLAInfo, regionExt, typeS1, typeS2) {
 
@@ -30,8 +67,8 @@ getSeqCMP <- function(HLAInfo, regionExt, typeS1, typeS2) {
     posS1 <- getTypingPos(HLAInfo$HLAAlignment, splitS1)
     posS2 <- getTypingPos(HLAInfo$HLAAlignment, splitS2)
 
-    if(splitS1[1] != splitS2[1]){
-        stop("Call get seq with type from 2 genes")
+    if (splitS1[1] != splitS2[1]) {
+        stop("The typing for the two alleles must be for the same HLA gene.")
     }
 
     refSeq <- HLAInfo$refSeq[[splitS1[1]]]
@@ -67,13 +104,13 @@ getSeqCMP <- function(HLAInfo, regionExt, typeS1, typeS2) {
 #' ## TODO
 #'
 #' @author Pascal Belleau, Astrid Deschenes
-#' @importFrom data.table data.table rbindlist
+#' @importFrom data.table rbindlist
 #' @export
 getSubSeq <- function(seq, posInit, regionExt){
 
     subSeq <- ""
 
-    for(i in seq_len(nrow(regionExt))){
+    for(i in seq_len(nrow(regionExt))) {
         subSeq <- paste0(subSeq, substr(seq,
                             regionExt$start[i] - posInit,
                             regionExt$end[i] - posInit))
