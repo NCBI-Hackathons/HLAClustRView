76d23e2ebe4ce42836606ab935d836218b8360a0
Astrid Deschenes <adeschen@hotmail.com>
2019-03-19 20:35:34 -0400

    Adding unit test for print.HLAMetric() function

---
+++ tests/testthat/test-print_HLAMetric.R
@@ -0,0 +1,30 @@
+### Unit tests for functions in print.HLAMetric.R file
+
+library(HLAClustRView)
+
+
+data("demoHLADataset")
+
+
+### Tests print() for HLAMetric class
+
+context("print() HLAMetric class")
+
+test_that("print() for HLAMetric object must return identical object", {
+
+    expeced <- calculateHamming(demoHLADataset)
+
+    result <- print(expeced)
+
+    expect_equal(result, expeced)
+})
+
+test_that("print() for HLAMetric object without distance must return identical object", {
+
+    demo <- calculateHamming(demoHLADataset)
+    demo[["dist"]] <- NULL
+
+    result <- print(demo)
+
+    expect_equal(result, demo)
+})
